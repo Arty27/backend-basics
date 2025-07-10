822254bcdacb24249bca0e8f2f2938d8f07d95c7
ec24186 <vinayn.1999@gmail.com>
2025-07-11 00:11:15 +0100

    custom middleware and error handler written

---
+++ express-concepts/middlewares/errorHandler.js
@@ -0,0 +1,33 @@
+class APIError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = "API Error";
+  }
+}
+
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  console.log(err.stack);
+  if (err instanceof APIError) {
+    return res.staus(err.statusCode).json({
+      status: "Error",
+      message: err.message,
+    });
+  } else if (err.name === "validationError") {
+    return res.status(400).json({
+      status: "Error",
+      message: "Validation Error",
+    });
+  } else {
+    return res.status(500).json({
+      status: "Error",
+      message: "An Unexpected error occured!",
+    });
+  }
+};
+
+module.exports = { APIError, asyncHandler, globalErrorHandler };
--- express-concepts/server.js
+++ express-concepts/server.js
@@ -2,13 +2,20 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const { configureCors } = require("./config/corsConfig");
-
+const {
+  requestLogger,
+  addTimeStamp,
+} = require("./middlewares/customMiddleware");
+const { globalErrorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(requestLogger);
+app.use(addTimeStamp);
+
 app.use(configureCors());
 app.use(express.json());
-
+app.use(globalErrorHandler);
 app.listen(port, () => {
   console.log("Server is now running on port", port);
 });
